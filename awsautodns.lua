50e60cd765799e73b43182ccf2a9cd58ace2893e
walia6 <awalia2002@gmail.com>
2021-01-30 17:21:05 -0500

    Update awsautodns.lua

--- awsautodns.lua
+++ awsautodns.lua
@@ -1,4 +1,4 @@
-local json = require("/opt/awsautodns/json")
+local json = dofile("/opt/awsautodns/json.lua")
 
 local hexset = {"0" ,"1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"}
 
